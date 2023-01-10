6b4941f703f87e667d2a7079e4021e2396915d41
Pravallika Myneni <pravallika@pravallikas-MacBook-Pro.local>
2023-01-09 20:01:01 -0600

    pair link

--- generate.py
+++ generate.py
@@ -1,24 +1,22 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_World():
 
-#pyrosim.Send_Cube(name="Box", pos=[0,0,0] , size=[1,1,1])
-#pyrosim.Send_Cube(name="Box2", pos=[1,0,1] , size=[1,1,1])
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[4,1,1] , size=[1,1,1])
+    pyrosim.End()
 
-pos_x, pos_y, pos_z = 0,0,1
-size_ = [1,1,1]
+def Create_Robot():
+    
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="BackLeg", pos=[0,0,0.5], size=[1,1,1])
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="BackLeg", child="Torso", type="revolute", position=[0.0,0.5,1.0])
+    pyrosim.Send_Cube(name="Torso", pos=[0,0.5,0.5], size=[1,1,1])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.0,0.5,-0.5])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0,1.0,0.0], size=[1,1,1])
+    pyrosim.End()
+    
+    
+Create_World()
+Create_Robot()
 
-for tower_1 in range(5):
-    for tower_2 in range(5):
-        for tower_3 in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[pos_x, pos_y, pos_z] , size=size_)
-            pos_z +=1
-            print(size_)
-            size_ = [0.9 * _ for _ in size_]
-        pos_y = pos_y+1
-        pos_z = 1
-        size_ = [1,1,1]
-    pos_y = 0
-    pos_x +=1
-
-pyrosim.End()
